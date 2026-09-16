1e7e4f1427148231b4e837b4363aa0bd8444d90f
AnantSrivastava-git <anantsrivastava65@gmail.com>
2026-09-17 00:45:01 +0530

    feat: implement user authentication and registration functionality with API integration

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,28 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: true,
+  baseURL: "http://localhost:8000/",
+//   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default api;
+
